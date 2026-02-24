41ff67951a3f602907af1c8f0b3ce11ba9c85a84
kazu1999 <squid.7171@gmail.com>
2026-02-24 17:11:31 +0900

    aaa

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import add, subtract
+from main import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -7,3 +7,7 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(2, 5) == -3
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
